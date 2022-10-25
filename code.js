dac5980b47632b6dfbab751f8bf4b764b410a9c6
Calathea-Z <zsykes21@gmail.com>
2022-10-25 10:33:11 -0400

    Begin set up of framework for game.

---
+++ code.js
@@ -0,0 +1,20 @@
+ const player = {
+    currentChoice: null
+ }
+
+ const computer = {
+    currentChoice: null
+ }
+ const choices = ["rock", "paper", "scissors"];
+   
+   
+   
+   // This code block find a random number between 0-2 which acts as the computer's choice.
+    function getComputerChoice(x){
+      x=3;
+      let z = Math.floor(Math.random() * x);
+      return 
+    };
+
+    computerChoice == [(getComputerChoice())];
+console.log(computerChoice);
