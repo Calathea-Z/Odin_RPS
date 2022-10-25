25f3088ce0d51231b9bff75799fe5a17b04bdbee
Calathea-Z <zsykes21@gmail.com>
2022-10-25 13:45:58 -0400

    update the getComputerChoice() function

--- code.js
+++ code.js
@@ -1,20 +1,20 @@
- const player = {
-    currentChoice: null
- }
+            // set the variables for the game //
+            
 
- const computer = {
-    currentChoice: null
- }
+ const computer = {currentChoice: null};
+
+ const player = {currentChoice: null};
+
  const choices = ["rock", "paper", "scissors"];
    
    
-   
-   // This code block find a random number between 0-2 which acts as the computer's choice.
-    function getComputerChoice(x){
-      x=3;
-      let z = Math.floor(Math.random() * x);
-      return 
+           // This code block finds a random number between 0-2 which acts as the computer's choice. //
+
+    function getComputerChoice(){
+      const randomIndex = Math.floor(Math.random() * 3);
+      computer.currentChoice = choices[randomIndex];
+      return choices[randomIndex];
     };
 
-    computerChoice == [(getComputerChoice())];
-console.log(computerChoice);
+   
+    alert(getComputerChoice());
